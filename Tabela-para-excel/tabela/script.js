7f8979d23e1bd813f9a3cd769a8a85e3fe15b5fb
mekafuyu <mayconbrunob@hotmail.com>
2024-04-03 10:18:57 -0300

    adicionando colunas tabela

--- Tabela-para-excel/tabela/script.js
+++ Tabela-para-excel/tabela/script.js
@@ -1,11 +1,13 @@
 function adicionarItem() {
   // Obter os valores dos campos de entrada
   var nome = document.getElementById("nome").value;
+  var fornecedor = document.getElementById("fornecedor").value;
   var valor = document.getElementById("valor").value;
+  var estado = document.getElementById("estado").value;
   var quantidade = document.getElementById("quantidade").value;
 
   // Validar se os campos estão preenchidos
-  if (!nome || !valor || !quantidade) {
+  if (!nome || !fornecedor || !valor || !estado || !quantidade) {
     alert("Preencha todos os campos.");
     return;
   }
@@ -16,17 +18,23 @@ function adicionarItem() {
     .getElementsByTagName("tbody")[0];
   var novaLinha = tabela.insertRow(tabela.rows.length);
   var celulaNome = novaLinha.insertCell(0);
-  var celulaValor = novaLinha.insertCell(1);
-  var celulaQuantidade = novaLinha.insertCell(2);
+  var celulaFornecedor = novaLinha.insertCell(1);
+  var celulaValor = novaLinha.insertCell(2);
+  var celulaEstado = novaLinha.insertCell(3);
+  var celulaQuantidade = novaLinha.insertCell(4);
 
   // Preencher as células com os valores
   celulaNome.innerHTML = nome;
+  celulaFornecedor.innerHTML = fornecedor;
   celulaValor.innerHTML = valor;
+  celulaEstado.innerHTML = estado;
   celulaQuantidade.innerHTML = quantidade;
 
   // Limpar os campos de entrada
   document.getElementById("nome").value = "";
+  document.getElementById("fornecedor").value = "";
   document.getElementById("valor").value = "";
+  document.getElementById("estado").value = "";
   document.getElementById("quantidade").value = "";
 }
 
